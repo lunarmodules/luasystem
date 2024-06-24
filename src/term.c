a7ed5242a797383b82bd48e1fe6fc20d544ed6e0
Thijs <thijs@thijsschreijer.nl>
2024-06-24 20:21:37 +0200

    fix(*): msys2 requires conio.h header

--- src/term.c
+++ src/term.c
@@ -12,13 +12,14 @@
 #include "compat.h"
 #include "bitflags.h"
 
-#ifndef _MSC_VER
-# include <unistd.h>
-#endif
 
 #ifdef _WIN32
 # include <windows.h>
 # include <locale.h>
+# ifndef _MSC_VER
+#  include <conio.h>
+#  include <unistd.h>
+# endif
 #else
 # include <termios.h>
 # include <string.h>
