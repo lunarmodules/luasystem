dfd0d4b8ca3607ae39b1d2cbad4e3a7180dd6754
Marc Jakobi <marc@jakobi.dev>
2026-01-21 22:40:19 +0100

    fix(compat): duplicate symbol when linking with luajit on windows (#81)

--- src/compat.h
+++ src/compat.h
@@ -4,7 +4,7 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-#if LUA_VERSION_NUM == 501
+#if LUA_VERSION_NUM == 501 && !defined(LUAJIT_VERSION)
 void luaL_setfuncs(lua_State *L, const luaL_Reg *l, int nup);
 void *luaL_testudata(lua_State *L, int ud, const char *tname);
 #endif
